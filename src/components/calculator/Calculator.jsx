2e20bd675314ff128e0402e71081be0ddb5ae3e2
Eric Oliveira <oiericoi@hotmail.com>
2022-09-09 15:15:17 -0300

    criado componentes button e calculator

---
+++ src/components/calculator/Calculator.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from '../button/Button'
+
+import './Calculator.css'
+
+function Calculator() {
+  return (
+    <div className='calculator'>
+      <Button name="1"/>
+      <Button name="1"/>
+      <Button name="1"/>
+      <Button name="1"/>
+      <Button name="1"/>
+    </div>
+  )
+}
+
+export default Calculator
