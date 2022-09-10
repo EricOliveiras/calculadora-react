b070b56ade48796fd68f13f1ef571d0e7a2895a1
Eric Oliveira <oiericoi@hotmail.com>
2022-09-10 13:33:30 -0300

    avançando na logica da calculadora

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import './Button.css'
 
 function Button(props) {
+  const handleClick = (e) => {
+    e.preventDefault()
+    props.click && props.click(props.label)
+  }
+
   return (
-    <button className='button'>
-      {props.name}
+    <button className={`
+      button
+      ${props.operation ? 'operation' : ''}
+      ${props.double ? 'double' : ''}
+      ${props.triple ? 'triple' : ''}
+    `}
+      onClick={handleClick}
+    >
+      {props.label}
     </button>
   )
 }
--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -1,16 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../button/Button'
+import Display from '../display/Display'
 
 import './Calculator.css'
 
 function Calculator() {
+  const [value, setValue] = useState(['0'])
+
+  const clearMemory = () => {
+    setValue('0')
+  }
+
+  const setOperation = (operation) => {
+    console.log(operation)
+  }
+
+  const addDigit = (n) => {
+    if (n === '.' && value.includes('.')) return
+
+    const clearDisplay = value === '0'
+    const currentValue = clearDisplay ? '' : value
+    const displayValue = currentValue + n
+
+    if(n !== '.') {
+      setValue(parseFloat(displayValue))
+    }
+  }
+
   return (
     <div className='calculator'>
-      <Button name="1"/>
-      <Button name="1"/>
-      <Button name="1"/>
-      <Button name="1"/>
-      <Button name="1"/>
+      <Display value={value} />
+      <Button label="AC" triple click={clearMemory} />
+      <Button label="/" operation click={setOperation} />
+      <Button label="7" click={addDigit} />
+      <Button label="8" click={addDigit} />
+      <Button label="9" click={addDigit} />
+      <Button label="*" operation click={setOperation} />
+      <Button label="4" click={addDigit} />
+      <Button label="5" click={addDigit} />
+      <Button label="6" click={addDigit} />
+      <Button label="-" operation click={setOperation} />
+      <Button label="1" click={addDigit} />
+      <Button label="2" click={addDigit} />
+      <Button label="3" click={addDigit} />
+      <Button label="+" operation click={setOperation} />
+      <Button label="0" double click={addDigit} />
+      <Button label="." click={addDigit} />
+      <Button label="=" operation click={setOperation} />
     </div>
   )
 }
